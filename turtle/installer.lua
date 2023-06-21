4a899438f1665fad30c9e0eea244648dc4c584de
TenCreator <lucakinsey@gmail.com>
2023-06-21 15:40:19 -0500

    Update installer.lua

--- turtle/installer.lua
+++ turtle/installer.lua
@@ -9,33 +9,44 @@ local Urls = {
 local deviceType = os.getComputerLabel()
 
 function installFile(file)
-    if not fs.isDir('pj-invade') then shell.run('mkdir pj-invade') end
+    if not fs.isDir('pj-invade') then shell.run('mkdir /pj-invade') end
 
-    if (fs.exists('pj-invade/config.lua')) then
+    if (fs.exists('/pj-invade/config.lua')) then
         print('Backing up config file to config.lua.bk')
         if (fs.exists('pj-invade/config.lua.bk')) then
             print('Deleting old backup config')
-            fs.delete('pj-invade/config.lua.bk')
+            fs.delete('/pj-invade/config.lua.bk')
             print('Deleted backup config')
         end
         print('backing up config file')
-        fs.copy('pj-invade/config.lua', 'pj-invade/config.lua.bk')
+        fs.copy('/pj-invade/config.lua', '/pj-invade/config.lua.bk')
         print('Config backed up!')
-        fs.delete('pj-invade/config.lua')
+        fs.delete('/pj-invade/config.lua')
     end
     print('Installing the config.lua')
-    shell.run('wget '..Urls.config.. ' pj-invade/config.lua')
+    shell.run('wget '..Urls.config.. ' /pj-invade/config.lua')
     print('config.lua installed successfully')
 
-    if (fs.exists('pj-invade/'..file..'.lua')) then
-        if (fs.exists('pj-invade/'..file..'.lua.bk')) then
-            fs.delete('pj-invade/'..file..'.lua.bk')
+    if (fs.exists('/pj-invade/'..file..'.lua')) then
+        if (fs.exists('/pj-invade/'..file..'.lua.bk')) then
+            fs.delete('/pj-invade/'..file..'.lua.bk')
         end
-        fs.copy('pj-invade/'..file..'.lua', 'pj-invade/'..file..'.lua.bk')
-        fs.delete('pj-invade/'..file..'.lua')
+        fs.copy('/pj-invade/'..file..'.lua', 'pj-invade/'..file..'.lua.bk')
+        fs.delete('/pj-invade/'..file..'.lua')
     end
 
-    if string.upper(file) =='ROUTER' then shell.run('wget '..Urls.ws.. ' pj-invade/ws.lua') end
+    if string.upper(file) =='ROUTER' then
+    
+    if (fs.exists('/pj-invade/ws.lua')) then
+        if (fs.exists('/pj-invade/ws.lua.bk')) then
+            fs.delete('/pj-invade/ws.lua.bk')
+        end
+        fs.copy('/pj-invade/ws.lua', '/pj-invade/ws.lua.bk')
+        fs.delete('/pj-invade/ws.lua')
+    end
+    
+    shell.run('wget '..Urls.ws.. ' /pj-invade/ws.lua')
+    end
 
     print('Installing the '..file..'.lua')
     shell.run('wget '..Urls[file].. ' pj-invade/'..file..'.lua')
