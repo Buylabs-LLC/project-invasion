dfc30627e96e65f4121102571ba00abffc0a727d
TenCreator <lucakinsey@gmail.com>
2023-06-21 15:48:58 -0500

    Update turtle.lua

--- turtle/turtle.lua
+++ turtle/turtle.lua
@@ -1,6 +1,6 @@
 turtle = {}
 local tId = 0
-local config = require('config')
+local config = require('/pj-invade/config')
 local routerStatus = 'Waiting for response'
 local network = rednet.lookup(config.network)
 local router = rednet.lookup(config.network, 'main-router')
