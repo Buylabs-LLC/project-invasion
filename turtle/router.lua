3111fd0587219cf0091236b2863411371b8ddb74
TenCreator <lucakinsey@gmail.com>
2023-06-22 18:07:47 +0100

    this might work or I might cry

--- turtle/router.lua
+++ turtle/router.lua
@@ -1,10 +1,6 @@
 local Config = require('/PJ-Invade/config')
 
 -- Initialize router with network
-function run()
-    rednet.open('bottom')
-    rednet.host('pj-invade', 'main-router')
-    print('Router Initialize')
-end
-
-return run
+rednet.open('bottom')
+rednet.host('pj-invade', 'main-router')
+print('Router Initialize')
