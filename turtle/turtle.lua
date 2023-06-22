ceeb2dc1a116e1f14e68e52cb49a019ce0a5a22b
TenCreator <lucakinsey@gmail.com>
2023-06-22 20:07:01 +0100

    updated the Turtle.lua so the updater actually runs every 60 seconds

--- turtle/turtle.lua
+++ turtle/turtle.lua
@@ -8,53 +8,40 @@ local updater = require('/PJ-Invade/updater')
 local routerStatus = 'Waiting for response'
 local network = rednet.lookup(config.network)
 local router = rednet.lookup(config.network, 'main-router')
+updater()
 
 local function checkIfRouterIsRunning()
     local WaitTime = 30 -- Seconds
     local firstCheck = false
     local connectDown = false
-    while true do
-        print(config.network)
-        print(network)
-        print(router)
-        if router then
-            if not firstCheck then
-                routerStatus = 'Successfully established a connection to the main router!'
-                print('Successfully established a connection to the main router!')
-                connectDown = false
-                firstCheck = true
-            elseif connectDown then
-                connectDown = false
-                routerStatus = 'Successfully established a connection to the main router!'
-                print('Connection to the router has been restored!')
-            end
-        else
-            routerStatus = 'Failed to establish a connection to the main router!'
-            print('Failed to establish a connection to the main router!')
-            connectDown = true
+    print(config.network)
+    print(network)
+    print(router)
+    if router then
+        if not firstCheck then
+            routerStatus = 'Successfully established a connection to the main router!'
+            print('Successfully established a connection to the main router!')
+            connectDown = false
+            firstCheck = true
+        elseif connectDown then
+            connectDown = false
+            routerStatus = 'Successfully established a connection to the main router!'
+            print('Connection to the router has been restored!')
         end
-        os.sleep(WaitTime)
+    else
+        routerStatus = 'Failed to establish a connection to the main router!'
+        print('Failed to establish a connection to the main router!')
+        connectDown = true
     end
-end
-
-local function setId()
-    print('There is '..#network..' devices registered on the network')
-    tId = #network + 1
+    os.sleep(WaitTime)
 end
 
 turtle.getInven = function()
-    
+ 
 end
 
-checkIfRouterIsRunning()
-setId()
-rednet.host(config.network, tId)
-os.setComputerLabel(tId.. ' Turtle')
-print('Turtle ID: '..tId)
-
-
-
 while true do
+    checkIfRouterIsRunning()
     os.sleep(60)
     updater()
     -- status()
