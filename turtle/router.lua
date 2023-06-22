f7f84df317f005c991b599b1ba131d90a9bc8c30
TenCreator <lucakinsey@gmail.com>
2023-06-22 17:50:04 +0100

    Removed checking for a wireless modem and fixed other issues

--- turtle/router.lua
+++ turtle/router.lua
@@ -1,17 +1,4 @@
--- does not crash
-local sides = {'left', 'right', 'back', 'top', 'bottom'}
-
-for _, side in pairs(sides) do
-    if peripheral.isPresent(side) and peripheral.getType(side) == "modem" then
-        print("Wireless modem found!")
-    else
-        print("No wireless modem detected.")
-        return
-    end
-end
-
 local Config = require('/PJ-Invade/config')
-
 
 function run()
     -- Initialize router with network
@@ -20,6 +7,5 @@ function run()
 
 
 end
-
 
 return run
