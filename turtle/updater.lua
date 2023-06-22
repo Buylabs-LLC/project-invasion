5394f5481162f581c00552c5924af2b5645d91de
TenCreator <lucakinsey@gmail.com>
2023-06-22 14:10:25 +0100

    last one didn't fix it so I used ChatGBP this time

--- turtle/updater.lua
+++ turtle/updater.lua
@@ -14,7 +14,7 @@ local function checkForUpdates()
     ["Authorization"] = "Bearer " .. authToken
   }
   local response = http.get(apiUrl, headers)
-  local commitData = textutils.unserialiseJSON(response.readAll())
+  local commitData = textutils.unserializeJSON(response.readAll())
   local latestCommitHash = commitData.sha
 
   local currentCommitFile = computerPath .. "/current_commit.txt"
@@ -32,19 +32,21 @@ local function checkForUpdates()
     local treeApiUrl = string.format("https://api.github.com/repos/%s/%s/git/trees/%s?recursive=true", repoOwner, repoName, latestCommitHash)
     local treeResponse = http.get(treeApiUrl, headers)
     local treeData = textutils.unserializeJSON(treeResponse.readAll())
-    local files = treeData.tree
 
-    for _, fileData in ipairs(files) do
-      local filePath = fileData.path
-      if filePath:find(repoDirectory) == 1 and fileData.type == "blob" then
-        local fileName = filePath:sub(#repoDirectory + 2) -- Remove the repoDirectory prefix
-        local downloadUrl = fileData.url:gsub("/blob/", "/raw/") -- Convert URL to raw format
-        local fileContent = http.get(downloadUrl).readAll()
-        local file = io.open(computerPath .. "/" .. fileName, "w")
-        file:write(fileContent)
-        file:close()
+    -- Iterate over the tree elements to find the files
+    for _, element in ipairs(treeData.tree) do
+      if element.type == "blob" then
+        local filePath = element.path
+        if filePath:find(repoDirectory) == 1 then
+          local fileName = filePath:sub(#repoDirectory + 2) -- Remove the repoDirectory prefix
+          local downloadUrl = element.url:gsub("/blob/", "/raw/") -- Convert URL to raw format
+          local fileContent = http.get(downloadUrl).readAll()
+          local file = io.open(computerPath .. "/" .. fileName, "w")
+          file:write(fileContent)
+          file:close()
 
-        print("Updated: " .. fileName)
+          print("Updated: " .. fileName)
+        end
       end
     end
 
