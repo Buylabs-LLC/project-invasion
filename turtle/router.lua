c5622f1303c1a7c85d425acfa9c5365c7e93f932
TenCreator <lucakinsey@gmail.com>
2023-06-22 18:21:54 +0100

    now accepting messages

--- turtle/router.lua
+++ turtle/router.lua
@@ -1,6 +1,19 @@
 local Config = require('/PJ-Invade/config')
+local status = require('/PJ-Invade/status')
 
 -- Initialize router with network
 rednet.open('bottom')
 rednet.host('pj-invade', 'main-router')
-print('Router Initialize')
+print('Router Initialized')
+
+
+
+
+local req
+while true do
+    req = rednet.request()
+    if req then
+        print(req)
+    end
+    -- status()
+end
