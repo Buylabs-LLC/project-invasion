c76a661127947e6b61b009d29ab25f38242f601b
TenCreator <lucakinsey@gmail.com>
2023-06-22 18:28:09 +0100

    changed a value in the router

--- turtle/router.lua
+++ turtle/router.lua
@@ -1,9 +1,9 @@
-local Config = require('/PJ-Invade/config')
+local config = require('/PJ-Invade/config')
 -- local status = require('/PJ-Invade/status')
 
 -- Initialize router with network
 rednet.open('bottom')
-rednet.host('pj-invade', 'main-router')
+rednet.host(config.network, 'main-router')
 print('Router Initialized')
 
 
