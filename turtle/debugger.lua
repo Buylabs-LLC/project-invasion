1d01ff224b93bb2b28209597a9bacece0757a89b
TenCreator <lucakinsey@gmail.com>
2023-06-24 16:35:36 +0100

    debugger

--- turtle/debugger.lua
+++ turtle/debugger.lua
@@ -1,6 +1,6 @@
 function debugger(msg)
     local monitor = peripheral.find('monitor')
-    local log = io.open('log', "w+")
+    local log = io.open('log', "w")
 
     file:write(msg)
     file:close()
