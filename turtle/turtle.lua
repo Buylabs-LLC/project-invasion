51fd7004402e71ac3e47392cd38158d31dcc2a19
TenCreator <lucakinsey@gmail.com>
2023-06-24 15:23:02 +0100

    removed a useless variable

--- turtle/turtle.lua
+++ turtle/turtle.lua
@@ -2,7 +2,7 @@ turtle = {}
 local config = require('/PJ-Invade/config')
 local updater = require('/PJ-Invade/updater')
 -- local status = require('/PJ-Invade/status')
-local routerStatus, router, firstCheck, connectDown = 'Waiting for response', rednet.lookup(config.network, 'main-router'), false, false
+local router, firstCheck, connectDown = rednet.lookup(config.network, 'main-router'), false, false
 updater()
 
 peripheral.find("modem", rednet.open)
@@ -10,17 +10,14 @@ peripheral.find("modem", rednet.open)
 local function checkIfRouterIsRunning()
     if router then
         if not firstCheck then
-            routerStatus = 'Successfully established a connection to the main router!'
             print('Successfully established a connection to the main router!')
             connectDown = false
             firstCheck = true
         elseif connectDown then
             connectDown = false
-            routerStatus = 'Successfully established a connection to the main router!'
             print('Connection to the router has been restored!')
         end
     else
-        routerStatus = 'Failed to establish a connection to the main router!'
         print('Failed to establish a connection to the main router!')
         connectDown = true
     end
