2f5786483b402f01cf2b2bad2ab2ec5b85d02ad9
TenCreator <lucakinsey@gmail.com>
2023-06-24 15:55:18 +0100

    added a last pinged selector

--- turtle/router.lua
+++ turtle/router.lua
@@ -9,18 +9,6 @@ peripheral.find("modem", rednet.open)
 rednet.host(config.network, 'main-router')
 print('Router Initialized')
 
-function checkTurtles()
-    for k, v in pairs(turtles) do
-        local isUp = rednet.send(v.id, 'Checkup')
-        print(isUp)
-        if not isUp then
-            turtles[k].active = false
-        end
-    end
-
-    print(textutils.serialiseJSON(turtles))
-end
-
 local id, msg, strReq
 while true do
     updater()
@@ -30,11 +18,13 @@ while true do
     id, msg, strReq = rednet.receive()
     if msg then
         if not turtles[id] then
-            turtles[id] = {id = id, active = true}
+            turtles[id] = {id = id, lastmsg = msg, lastpinged = os.time('gmt')}
             print('Added a new turtle to the local db')
             print(textutils.serialiseJSON(turtles))
         end
-        print('MSG')
-        print(msg)
+        turtles[id].lastmsg = msg
+        turtles[id].lastpinged = os.time('gmt')
+        print('last pinged')
+        print(turtles[id].lastpinged)
     end
 end
