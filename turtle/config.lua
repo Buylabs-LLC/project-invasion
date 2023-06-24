6872d9d8f83d95e2865c03231971a568436ae4f8
TenCreator <lucakinsey@gmail.com>
2023-06-24 19:26:45 +0100

    fixed the config crashing shit

--- turtle/config.lua
+++ turtle/config.lua
@@ -6,16 +6,6 @@ Config['WSS'] = 'WSS://localhost:5454'
 Config['modem'] = {}
 Config['modem']['port'] = 57452
 
-Config['downloadUrls'] = {
-    MASTER = baseUrl .. "/" .. folderPath .. "/master.lua",
-    ROUTER = baseUrl .. "/" .. folderPath .. "/router.lua",
-    TURTLE = baseUrl .. "/" .. folderPath .. "/turtle.lua",
-    UPDATER = baseUrl .. "/" .. folderPath .. "/updater.lua",
-    CONFIG = baseUrl .. "/" .. folderPath .. "/config.lua",
-    STATUS = baseUrl .. "/" .. folderPath .. "/status.lua",
-    DEBUGGER = baseUrl .. "/" .. folderPath .. "/debugger.lua"
-}
-
 Config['gitInfo'] = {}
 Config['gitInfo']['repoOwner'] = 'BuyLabs-LLC'
 Config['gitInfo']['repoName'] = 'project-invasion'
