f6403798db685eccecbee3b790cef33616361a8f
TenCreator <lucakinsey@gmail.com>
2023-06-26 16:17:15 +0100

    trying an new way of running things repeatedly

--- turtle/router.lua
+++ turtle/router.lua
@@ -3,12 +3,43 @@ local updater = require('/PJ-Invade/updater')
 updater()
 local config, debug = require('/PJ-Invade/config'), require('/PJ-Invade/debugger')
 local turtles, masters, contact = {}, {}, {}
+local id, msg, strReq
 
 -- Initialize router with network
 peripheral.find("modem", rednet.open)
 rednet.host(config.network, 'main-router')
 print('Router Initialized')
 debug('Router Initialized', 'success')
+
+local function checkForResponse()
+    id, msg, strReq = rednet.receive()
+    if msg then
+        if string.upper(strReq) == 'TURTLE' then
+            if not turtles[id] then
+                turtles[id] = {id = id, lastmsg = msg, lastpinged = os.time('utc'), active = true, inactitity = 0}
+                debug('Added a new turtle to the local db', 'success')
+            else
+                turtles[id].lastmsg = msg
+                turtles[id].lastpinged = os.time('utc')
+            end
+            -- Add handler here!
+        elseif string.upper(strReq) == 'MASTER' then
+            if not masters[id] then
+                masters[id] = {id = id, lastmsg = msg, lastpinged = os.time('utc'), active = true, inactitity = 0}
+                debug('Added a new master to the local db', 'succ')
+            else
+                masters[id].lastmsg = msg
+                masters[id].lastpinged = os.time('utc')
+            end
+            -- Add handler here!
+        elseif strReq == 'dns' then
+            debug('DNS Query recived by '..id, 'dns')
+        else
+            debug('Contacted by an registered party', 'err')
+            debug(strReq, 'err')
+        end
+    end
+end
 
 local function checkActive()
     debug('Activity Checking Initiated', 'info')
@@ -65,40 +96,6 @@ contact.master = function(fnc)
     end
 end
 
-local id, msg, strReq
-while true do
+local function no_sleep() sleep(0) end
 
-    debug('New set of data', 'PURPLE')
-
-    updater()
-    -- status()
-    checkActive()
-
-    id, msg, strReq = rednet.receive()
-    if msg then
-        if string.upper(strReq) == 'TURTLE' then
-            if not turtles[id] then
-                turtles[id] = {id = id, lastmsg = msg, lastpinged = os.time('utc'), active = true, inactitity = 0}
-                debug('Added a new turtle to the local db', 'success')
-            else
-                turtles[id].lastmsg = msg
-                turtles[id].lastpinged = os.time('utc')
-            end
-            -- Add handler here!
-        elseif string.upper(strReq) == 'MASTER' then
-            if not masters[id] then
-                masters[id] = {id = id, lastmsg = msg, lastpinged = os.time('utc'), active = true, inactitity = 0}
-                debug('Added a new master to the local db', 'succ')
-            else
-                masters[id].lastmsg = msg
-                masters[id].lastpinged = os.time('utc')
-            end
-            -- Add handler here!
-        elseif strReq == 'dns' then
-            debug('DNS Query recived by '..id, 'dns')
-        else
-            debug('Contacted by an registered party', 'err')
-            debug(strReq, 'err')
-        end
-    end
-end
+waitForAny(no_sleep, updater, checkActive, checkForResponse)
