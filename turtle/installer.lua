a29d12da0ac917d496953774284a9bfaeace5048
TenCreator <lucakinsey@gmail.com>
2023-06-28 16:43:24 +0100

    editing the startup script to use multishell with full compatability!

--- turtle/installer.lua
+++ turtle/installer.lua
@@ -54,7 +54,7 @@ local function createStartupScript(filePath)
     
     local id
     for _, k in pairs(scripts) do
-      id = multishell.launch({shell = shell, multishell = multishell, require = require}, k.location)
+      id = multishell.launch({shell = shell, multishell = multishell, require = require, rednet = rednet, http = http, peripheral = peripheral}, k.location)
       print('Launching '..k.title)
       multishell.setTitle(id, k.title)
     end
